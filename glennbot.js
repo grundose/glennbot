b588b452b3cafff5a43e912aa40a411b5db81ffb
Tom Keller <thetomkeller@gmail.com>
2017-05-04 18:02:00 -0400

    Added package.json for npm compatibility. Updated bot to use nconf

--- glennbot.js
+++ glennbot.js
@@ -1,26 +1,32 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
- 
-client.on('ready', () => {
-  console.log('I am ready!');
-});
+const nconf = require('nconf');
 
-//Verifies and pulls in auth.json
-try {
-	var AuthDetails = require("./auth.json");
-} catch (e){
-	console.log("Please create an auth.json like auth.json.example with a bot token or an email and password.\n"+e.stack);
-	process.exit();
-} 
+//Load configuration, either from environment or file
+nconf.argv()
+  .env()
+  .file({ file: './auth.json' });
 
+//Validate configuration
+if(typeof nconf.get('bot_token') === 'undefined' || typeof nconf.get('bot_token') !== 'string') {
+  console.error("No token information supplied.\n\nHint: try --bot_token TOKEN or set bot_token variable in auth.json");
+  process.exit(1);
+}
 
 //Authenticates
-if(AuthDetails.bot_token){
-	console.log("logging in with token");
-	client.login(AuthDetails.bot_token);
-} else {
-	console.log("Logging in with user credentials is no longer supported!\nYou can use token based log in with a user account, see\nhttps://discord.js.org/#/docs/main/master/general/updating");
-}
+process.stdout.write('Logging in to discord with token... ');
+client.login(nconf.get('bot_token'))
+  .then(console.log('GREAT SUCCESS!'))
+  .catch(function(err) {
+    console.log('FAILED!');
+    console.error(err);
+    process.exit(1);
+  });
+
+//Discord client events
+client.on('ready', () => {
+  console.log('Bot is authorized.');
+});
 
 //Tells trent to shutup when he says something
 client.on('message', message => {
@@ -36,8 +42,6 @@ client.on('message', message => {
   }
 });
 
-
-
 client.on('userconnection', userconnection => {
   if (userconnection.name ==='grund') {
     message.send('No ones going to look at your link Trent');
@@ -45,8 +49,6 @@ client.on('userconnection', userconnection => {
   }
 });
 
-
 //Message Logging
 client.on('message', message => {
   console.log(message.content, message.author.username)});
-
