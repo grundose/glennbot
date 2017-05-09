a6b084ec51dc253397f29c423c05c36d985407b8
Eric <caokeric@gmail.com>
2017-05-08 17:45:59 -0700

    Check array length. Oops.

--- util/random.js
+++ util/random.js
@@ -1,5 +1,7 @@
 exports.randomArrayValue = function (arr) {
   if (Array.isArray(arr)) {
-    return arr[Math.floor(Math.random() * (Math.floor(arr.length - 1)))];
+    if (arr.length > 0) {
+      return arr[Math.floor(Math.random() * (Math.floor(arr.length - 1)))];
+    }
   }
 };
