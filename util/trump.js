951cd2e210caf7533590180e8e752dab3959a999
Eric <caokeric@gmail.com>
2017-07-02 23:18:15 -0700

    Command handler. Now uses isomorphic-fetch library for requests.

--- util/trump.js
+++ util/trump.js
@@ -1,36 +1,45 @@
-const Promise = require('bluebird');
-const https = require('https');
+// const https = require('https');
 
 
-function randomTrumpQuote() {
-  const options = {
-    host: `api.whatdoestrumpthink.com`,
-    port: 443,
-    path: '/api/v1/quotes/random',
-    method: 'GET'
-  };
-  return new Promise((resolve, reject) => {
-    let quotes = '';
-    const req = https.request( options, (res) => {
-      res.on('data', (data) => {
-        quotes += data;
-       // console.log (quotes);
-      });
-    });
-    req.on('close', () => {
-      const quoteResult = JSON.parse(quotes);
-      if (quoteResult.message.length === 0) return (resolve(`${subReddit} subreddit doesnt exist or its inactive.`));
-      console.log(quoteResult.message);
-      resolve(quoteResult.message)
-    });
-    req.on('error', (err) => {
-      reject(err);
-    });
-    req.end();
-  });
+// function randomTrumpQuote() {
+//     const options = {
+//         host: `api.whatdoestrumpthink.com`,
+//         port: 443,
+//         path: '/api/v1/quotes/random',
+//         method: 'GET'
+//     };
+//     return new Promise((resolve, reject) => {
+//         let quotes = '';
+//         const req = https.request(options, (res) => {
+//             res.on('data', (data) => {
+//                 quotes += data;
+//                 // console.log (quotes);
+//             });
+//         });
+//         req.on('close', () => {
+//             const quoteResult = JSON.parse(quotes);
+//             if (quoteResult.message.length === 0) return (resolve(`${subReddit} subreddit doesnt exist or its inactive.`));
+//             console.log(quoteResult.message);
+//             resolve(quoteResult.message)
+//         });
+//         req.on('error', (err) => {
+//             reject(err);
+//         });
+//         req.end();
+//     });
+// }
+
+const host = 'https://api.whatdoestrumpthink.com';
+
+function trumpQuote() {
+    return fetch(`${host}:443/api/v1/quotes/random`, { method: 'GET' })
+        .then(response => response.json())
+        .then(data => data.message)
+        .catch(error => { console.log(error) });
 }
 
-console.log(randomTrumpQuote());
 module.exports = {
-  quote: randomTrumpQuote,
+    trumpQuote: trumpQuote
 };
+
+
